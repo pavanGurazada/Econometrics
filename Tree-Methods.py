287eb75c20b6f517752bd920d297aa420755c7d4
pavanGurazada <pavankumar.gurazada@gmail.com>
2018-02-21 13:50:12 +0400

    setting things up

--- Tree-Methods.py
+++ Tree-Methods.py
@@ -1,14 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 20 17:17:47 2018
-
-@author: Pavan
-"""
-
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
-adult_train = pd.read_csv("data/")
-
-
+from sklearn.model_selection import train_test_split
