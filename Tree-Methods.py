398e5a1884f858841540f0484d088aa95d191113
pavanGurazada <pavankumar.gurazada@gmail.com>
2018-02-21 11:50:09 +0400

    munging data and fitting random forests

---
+++ Tree-Methods.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 17:17:47 2018
+
+@author: Pavan
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+adult_train = pd.read_csv("data/")
+
+
