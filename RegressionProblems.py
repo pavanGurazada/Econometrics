26b8a732bb0623770954d548637b575c654ecfe0
pavanGurazada <pavankumar.gurazada@gmail.com>
2018-02-25 13:32:40 +0400

    Initial ramblings on scikit learn

---
+++ RegressionProblems.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 25 12:28:14 2018
+
+@author: Pavan
+"""
+
+# Begin with toy problems and work your way through
+
+from sklearn import datasets
+
+iris = datasets.load_iris()
+digits = datasets.load_digits()
