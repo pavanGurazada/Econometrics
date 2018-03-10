7d1c464ef4c64a588eb68bfc06f7ece3516b1032
pavanGurazada <pavankumar.gurazada@gmail.com>
2018-03-10 09:16:09 +0400

    more examples...

--- Gujrati-example.R
+++ Gujrati-example.R
@@ -10,6 +10,9 @@ library(tidyverse)
 library(caret)
 library(boot)
 
+set.seed(20130810)
+theme_set(theme_bw())
+
 #' Though heavily biased towards Linear Regression, Econometrics deals with
 #' specifying the regression problems rooted on economic theory. The regression
 #' coefficient estimates and the sign of these estimates are derived from
@@ -146,3 +149,97 @@ corruption_model2 <- train(index ~ country * gdp_cap,
                            data = corruption,
                            method = "rf")
 corruption_model2
+
+#' *Example 6* Relationship between Gross Private Investments (GPI) and Gross
+#' Personal Savings (GPS). How much of the savings do people tend to invest?
+
+si_usa <- read.dta("data/gujrati-example/Stata/Table3_6.dta")
+glimpse(si_usa)
+
+summary(lm(gpi ~ gps, data = si_usa))
+
+#' 1981-82 was the worst recession for peace-time US. We can include this as a
+#' structural dummy using the variable recession81 = 1 for observations beyond
+#' 81 and 0 otherwise
+
+summary(lm(gpi ~ gps + recession81, data = si_usa))
+summary(lm(gpi ~ gps + recession81 + gpsrec81, data = si_usa))
+
+#' Results dont tally with the book but an interesting application of dummy
+#' variables to detect structural changes. This is especially true for
+#' time-series
+
+#' *Example 7*
+#' Deseasonalizing trends with dummy variables
+
+sales_data <- read.dta("data/gujrati-example/Stata/Table3_10.dta")
+glimpse(sales_data)
+
+summary(sales_model <- lm(sales ~ d2 + d3 + d4, data = sales_data))
+
+(sales_data$sales_adj <- mean(sales_data$sales) + residuals(sales_model))
+
+dev.new()
+ggplot(data = sales_data, aes(x = yearq, y = sales)) +
+  geom_line(aes(linetype = "Original data")) +
+  geom_line(aes(y = sales_adj, linetype = "Deseasonalized")) +
+  labs(x = "Year",
+       y = "Sales",
+       linetype = "Sales")
+
+#' As can be seen in the plot above, some of the variation in sales is
+#' attributed to the variation due to the demand associated with change in
+#' quarters. The underlying pattern beyond this seasonal variation can be
+#' explored using this kind of a regression analysis.
+
+summary(sales_model2 <- lm(sales ~ rpdi + conf + d2 + d3 + d4, 
+                           data = sales_data))
+
+sales_data$sales_adj <- mean(sales_data$sales) + residuals(sales_model2)
+
+ggplot(data = sales_data, aes(x = yearq)) +
+  geom_line(aes(y = sales, linetype = "Original Data")) +
+  geom_line(aes(y = sales_adj, linetype = "Deseasonalized")) +
+  labs(x = "Year",
+       y = "Sales",
+       linetype = "Sales")
+
+summary(sales_model3 <- lm(sales ~ rpdi + conf + d2 + d3 + d4 + 
+                                   d2:rpdi + d3:rpdi + d4:rpdi +
+                                   d2:conf + d3:conf + d4:conf,
+                           data = sales_data))
+
+#' From the above table, the lack of significance of the coefficients show that
+#' there is no seasonal variation in rpdi and conf
+
+summary(sales_model4 <- lm(log(sales) ~ rpdi + conf + d2 + d3 + d4, 
+                           data = sales_data))
+
+#' Verifying Frisch-Waugh theorem
+
+summary(s1 <- lm(sales ~ d2 + d3 + d4, data = sales_data))
+summary(s2 <- lm(rpdi ~ d2 + d3 + d4, data = sales_data))
+summary(s3 <- lm(conf ~ d2 + d3 + d4, data = sales_data))
+
+rs1 <- residuals(s1)
+rs2 <- residuals(s2)
+rs3 <- residuals(s3)
+
+summary(lm(rs1 ~ rs2 + rs3 - 1))
+summary(lm(sales ~ rpdi + conf + d2 + d3 + d4, data = sales_data))
+
+#' As can be seen from these regression outputs, the coefficients of rs2 and rs3
+#' in the first regression and rpdi and conf in the second regression are the
+#' same. This is nice since you do not need to account for seasonal variations
+#' in the outcome and the predictors individually. Usage of dummies achieves two
+#' tasks at once
+
+summary(lm(sales ~ rpdi + conf + d2 + d3 + d4 + trend, data = sales_data))
+
+#' *Example 8*
+#' 
+
+diabetes_data <- read.dta("data/gujrati-example/Stata/Table3_19.dta")
+glimpse(diabetes_data)
+
+summary(lm(diabetes ~ ban * sugar_sweet_cap, diabetes_data))
